3d25709f8b3623510cffd8afcc4fe8234c39d3e4
patrickpa <patrickabrahao.pa@gmail.com>
2018-09-28 21:15:40 -0300

    Initial files with working simulation

---
+++ out/gcc-release/src/TCPVideoStreamCliApp.d
@@ -0,0 +1,65 @@
+../out/gcc-release/src/TCPVideoStreamCliApp.o: TCPVideoStreamCliApp.cc \
+ TCPVideoStreamCliApp.h /home/patrick/omnetpp-5.3/include/omnetpp.h \
+ ../../inet/src/inet/common/INETDefs.h \
+ ../../inet/src/inet/common/precompiled_release.h \
+ ../../inet/src/inet/common/precompiled.h \
+ ../../inet/src/inet/opp_defines.h ../../inet/src/inet/features.h \
+ ../../inet/src/inet/common/Compat.h \
+ ../../inet/src/inet/common/InitStages.h \
+ ../../inet/src/inet/applications/tcpapp/TCPBasicClientApp.h \
+ ../../inet/src/inet/applications/tcpapp/TCPAppBase.h \
+ ../../inet/src/inet/transportlayer/contract/tcp/TCPSocket.h \
+ ../../inet/src/inet/transportlayer/contract/tcp/TCPCommand_m.h \
+ ../../inet/src/inet/networklayer/common/L3Address.h \
+ ../../inet/src/inet/networklayer/contract/ipv4/IPv4Address.h \
+ ../../inet/src/inet/networklayer/contract/ipv6/IPv6Address.h \
+ ../../inet/src/inet/linklayer/common/MACAddress.h \
+ ../../inet/src/inet/networklayer/common/ModuleIdAddress.h \
+ ../../inet/src/inet/networklayer/common/ModulePathAddress.h \
+ ../../inet/src/inet/common/lifecycle/NodeStatus.h \
+ ../../inet/src/inet/common/lifecycle/ILifecycle.h \
+ ../../inet/src/inet/applications/tcpapp/GenericAppMsg_m.h
+
+TCPVideoStreamCliApp.h:
+
+/home/patrick/omnetpp-5.3/include/omnetpp.h:
+
+../../inet/src/inet/common/INETDefs.h:
+
+../../inet/src/inet/common/precompiled_release.h:
+
+../../inet/src/inet/common/precompiled.h:
+
+../../inet/src/inet/opp_defines.h:
+
+../../inet/src/inet/features.h:
+
+../../inet/src/inet/common/Compat.h:
+
+../../inet/src/inet/common/InitStages.h:
+
+../../inet/src/inet/applications/tcpapp/TCPBasicClientApp.h:
+
+../../inet/src/inet/applications/tcpapp/TCPAppBase.h:
+
+../../inet/src/inet/transportlayer/contract/tcp/TCPSocket.h:
+
+../../inet/src/inet/transportlayer/contract/tcp/TCPCommand_m.h:
+
+../../inet/src/inet/networklayer/common/L3Address.h:
+
+../../inet/src/inet/networklayer/contract/ipv4/IPv4Address.h:
+
+../../inet/src/inet/networklayer/contract/ipv6/IPv6Address.h:
+
+../../inet/src/inet/linklayer/common/MACAddress.h:
+
+../../inet/src/inet/networklayer/common/ModuleIdAddress.h:
+
+../../inet/src/inet/networklayer/common/ModulePathAddress.h:
+
+../../inet/src/inet/common/lifecycle/NodeStatus.h:
+
+../../inet/src/inet/common/lifecycle/ILifecycle.h:
+
+../../inet/src/inet/applications/tcpapp/GenericAppMsg_m.h:
